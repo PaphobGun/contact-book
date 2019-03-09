1af9af3dba5dfea6a836c283497bb244e14acfc9
PaphobGun <paphob.a@gmail.com>
2019-03-09 16:42:16 +0700

    Updates

---
+++ precache-manifest.4c4f3519f083523764c12c5e59be5278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "176486221e95269698f0",
+    "url": "/contact-book/static/js/runtime~main.857aea94.js"
+  },
+  {
+    "revision": "402e890cda826c3ff857",
+    "url": "/contact-book/static/js/main.4185036c.chunk.js"
+  },
+  {
+    "revision": "b98cdb249720233897ce",
+    "url": "/contact-book/static/js/2.58e3aff9.chunk.js"
+  },
+  {
+    "revision": "402e890cda826c3ff857",
+    "url": "/contact-book/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "b98cdb249720233897ce",
+    "url": "/contact-book/static/css/2.d2c7de2b.chunk.css"
+  },
+  {
+    "revision": "c2906c822f5ccf8d61f918fd89e870b8",
+    "url": "/contact-book/index.html"
+  }
+];
